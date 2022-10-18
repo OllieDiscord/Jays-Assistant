7209d18febec73077f9af50d31e03fec4bece9b5
Jay <82115577+Not-Jayy@users.noreply.github.com>
2022-10-18 11:38:38 +0100

    Update waifu.js

--- src/commands/Misc/waifu.js
+++ src/commands/Misc/waifu.js
@@ -1,4 +1,4 @@
-const { CommandInteraction, MessageEmbed } = require("discord.js");
+const { CommandInteraction } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { PermissionFlagsBits } = require("discord-api-types/v10");
 const axios = require("axios");
@@ -7,7 +7,6 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName("waifu")
     .setDescription("Fetches a waifu (please kill me).")
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addBooleanOption((option) => option.setName("nsfw").setDescription("Is it NSFW?").setRequired(true)),
     /**
      * 
@@ -25,15 +24,13 @@ module.exports = {
             let data = response.data;
             const data_image = data.images[0].url;
 
-            const Embed_1 = new MessageEmbed().setColor("RED").setImage(`${data_image}`)
-            interaction.reply({ embeds: [Embed_1] });
+            interaction.reply({ content: `${data_image}` });
         } else {
             let response = await axios.get(`https://api.waifu.im/random/?is_nsfw=false`);
             let data = response.data;
             const data_image = data.images[0].url;
 
-            const Embed_2 = new MessageEmbed().setColor("YELLOW").setImage(`${data_image}`)
-            interaction.reply({ embeds: [Embed_2] });
+            interaction.reply({ content: `${data_image}` });
         };
     },
 };
