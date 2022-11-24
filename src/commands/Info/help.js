73af06000fc6ff59087708ec11ef9086dba12331
Jay <82115577+Not-Jayy@users.noreply.github.com>
2022-11-24 18:57:08 +0000

    Update help.js

--- src/commands/Info/help.js
+++ src/commands/Info/help.js
@@ -31,7 +31,7 @@ module.exports = {
             },
             {
                 name: "• Util",
-                value: "> `blacklist, debug, ping, ticket`"
+                value: "> `blacklist, debug, ping, ticket, eval`"
             }
         )
         
