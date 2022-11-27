c6338ef9ac184939ef59f1dc5e7391b8907e65c1
Jay <jayedmead2456@icloud.com>
2022-11-27 09:25:52 +0000

    yes

---
+++ src/core/systems/Server/BlockSystem.js
@@ -0,0 +1,10 @@
+const database = require("../../db/schemas/Systems/BlockModel.js");
+
+module.exports = (client) => {
+    client.on("messageCreate", async (message) => {
+        const Blocked = await database.findOne({ GuildID: guildId, UserID: message.author.id });
+        if (Blocked) {
+            await message.delete();
+        };
+    });
+};
