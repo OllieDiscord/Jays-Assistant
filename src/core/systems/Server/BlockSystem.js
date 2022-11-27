a9dcdf5b5f4e890375b464f57782db628a9677b3
Jay <jayedmead2456@icloud.com>
2022-11-27 09:36:43 +0000

    Update BlockSystem.js

--- src/core/systems/Server/BlockSystem.js
+++ src/core/systems/Server/BlockSystem.js
@@ -2,7 +2,7 @@ const database = require("../../db/schemas/Systems/BlockModel.js");
 
 module.exports = (client) => {
     client.on("messageCreate", async (message) => {
-        const Blocked = await database.findOne({ GuildID: guildId, UserID: message.author.id });
+        const Blocked = await database.findOne({ GuildID: message.guild.id, UserID: message.author.id });
         if (Blocked) await message.delete();
     });
 }
